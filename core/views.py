c3d385d6e543a9272f6b3d65d6f08361b852593a
Arthur Chang <arthurc0102@gmail.com>
2023-03-18 14:05:16 +0800

    改用 .get 取得 request 中的名字

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 
 
 def my_path(request):
-    name = request.GET['name']
+    name = request.GET.get('name', 'Unknown')
     return HttpResponse(f'<h1>Hello World {name}!!!</h1>')
