acdc58ab90acde3c51faae2725a103c752d54959
Arthur Chang <arthurc0102@gmail.com>
2023-03-18 14:17:25 +0800

    添加 form 用來送出 name

--- core/views.py
+++ core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def my_path(request):
     name = request.GET.get('name', 'Unknown')
-    return HttpResponse(f'<h1>Hello World {name}!!!</h1>')
+    return HttpResponse(f'<form method="GET"><input type="text" name="name"><input type="submit" value="send"></form><h1>Hello World {name}!!!</h1>')
