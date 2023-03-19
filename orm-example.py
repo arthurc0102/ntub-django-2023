52e8f7c999fc50a50780f56bf035fe9543bb0f35
Arthur Chang <arthurc0102@gmail.com>
2023-03-19 13:51:49 +0800

    Post 的 filter

--- orm-example.py
+++ orm-example.py
@@ -24,3 +24,17 @@ Post.objects.get(title='Title003')
 Post.objects.get(id=9, title='Title003')
 Post.objects.get(id=10, title='Title003')
 Post.objects.get(title='1111')
+
+# 查詢過濾 Post
+# 會傳傳多筆資料，如果需要第一筆可以用 [0] 或是 .first() 取得
+
+Post.objects.filter(id=9)  
+Post.objects.filter(title='Title003')
+Post.objects.filter(id=9, title='Title003')
+Post.objects.filter(id=10, title='Title003')
+Post.objects.filter(title='1111')
+
+Post.objects.filter(title='1111').first()
+Post.objects.filter(title='1111').last()
+Post.objects.filter(title='1111')[0]
+Post.objects.filter(title='1111')[0:2]
