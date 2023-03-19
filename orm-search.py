fa309e2a22cc651e10bc9e6baced0c1d42f098dd
Arthur Chang <arthurc0102@gmail.com>
2023-03-19 14:28:34 +0800

    Exclude

--- orm-search.py
+++ orm-search.py
@@ -6,3 +6,7 @@ Post.objects.filter(title='Title01')
 Post.objects.filter(title__contains='Title')
 
 Post.objects.filter(title__icontains='title')
+
+# 文件：https://docs.djangoproject.com/en/4.1/ref/models/querysets/#field-lookups
+
+Post.objects.exclude(title__icontains='title')
