0580abbb694703dc0300a78073c4314283e2095b
Arthur Chang <arthurc0102@gmail.com>
2023-04-29 09:54:44 +0800

    建立留言 Models

--- first/models.py
+++ first/models.py
@@ -10,3 +10,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=500)
+
+    # FK 刪除的策略
+    # models.CASCADE：連帶刪除 -> 刪除 Post 時一併刪除 Comment
+    # models.PROTECT：保護 -> 刪除 Post 時，若有 Comemnt 存在阻止 Post 刪除
+    # models.SET_DEFAULT：刪除 Post 時，將 Comment 中的 post 欄位設定成預設值
+    # models.SET_NULL：刪除 Post 時，將 Comment 中的 post 欄位設定成 null    
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
