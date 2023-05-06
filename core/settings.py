3fe3af36d0785031f45d5feb5ccd0f31bd21cbd2
Arthur Chang <arthurc0102@gmail.com>
2023-05-06 10:55:36 +0800

    Install and setup MySQL

--- core/settings.py
+++ core/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_dev',  # 資料庫名稱
+        'USER': 'root',
+        'PASSWORD': 'pwd',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
