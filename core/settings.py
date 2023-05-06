e7e0f4c4a460b6d4ce24d04c194b3bf97b74e845
Arthur Chang <arthurc0102@gmail.com>
2023-05-06 15:24:48 +0800

    還原回開發狀態

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-inr-1dtbt@^x)=li1x55g5#n=iv9km!$v^5qpimw5m8tq1jv#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # 允許來自所有 host 的請求
 
